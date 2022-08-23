f376ddec205c6204d579661afa30710c115c62e8
Ernest Addae <earnestaddae@gmail.com>
2022-08-23 09:48:56 +0800

    Setup application

---
+++ cmd/api/api.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+)
+
+const version = "0.0.1"
+
+type config struct {
+	port    int
+	env     string
+	version string
+}
+
+type application struct {
+	config   config
+	infoLog  *log.Logger
+	errorLog *log.Logger
+}
+
+func main() {
+	var cfg config
+	// Declare flags for the port, environment and version
+	flag.IntVar(&cfg.port, "port", 8080, "Server port to listen on")
+	flag.StringVar(&cfg.env, "env", "development", "Application environment (development|staging|production")
+	flag.StringVar(&cfg.version, "version", version, "Application version")
+	flag.Parse()
+
+	// Declare app logs
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// instantiate the application
+	app := &application{
+		config:   cfg,
+		infoLog:  infoLog,
+		errorLog: errorLog,
+	}
+
+	err := app.serve()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ cmd/api/server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func (app *application) serve() error {
+	srv := &http.Server{
+		Addr:              fmt.Sprintf(":%d", app.config.port),
+		Handler:           app.routes(),
+		IdleTimeout:       30 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      5 * time.Second,
+	}
+
+	app.infoLog.Printf("Starting backend server in %s mode on port %d\n", app.config.env, app.config.port)
+
+	return srv.ListenAndServe()
+}
